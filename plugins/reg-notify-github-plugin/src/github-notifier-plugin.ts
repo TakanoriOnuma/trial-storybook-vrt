3953ee083ff149d4b6f1b639abf4e0d59d12865e
大沼 峻徳 <numa.san.1024@gmail.com>
2024-06-15 19:01:35 +0900

    PRにコメントしてみる

--- plugins/reg-notify-github-plugin/src/github-notifier-plugin.ts
+++ plugins/reg-notify-github-plugin/src/github-notifier-plugin.ts
@@ -7,16 +7,29 @@ import {
 
 import fetch from 'node-fetch';
 
-export interface GitHubPluginOption {}
+export interface GitHubPluginOption {
+  /** PRにコメントするためのGitHub Token */
+  GITHUB_TOKEN: string;
+  /** コメント対象のリポジトリ(owner/repoName) */
+  GITHUB_REPOSITORY: string;
+  /** コメント対象のPR番号 */
+  GITHUB_PR_NUMBER: string;
+}
 
 export class GitHubNotifierPlugin
   implements NotifierPlugin<GitHubPluginOption>
 {
   private _logger!: Logger;
+  private _GITHUB_TOKEN!: string;
+  private _GITHUB_REPOSITORY!: string;
+  private _GITHUB_PR_NUMBER!: string;
 
   init(config: PluginCreateOptions<GitHubPluginOption>) {
     const { logger, options } = config;
     this._logger = logger;
+    this._GITHUB_TOKEN = options.GITHUB_TOKEN;
+    this._GITHUB_REPOSITORY = options.GITHUB_REPOSITORY;
+    this._GITHUB_PR_NUMBER = options.GITHUB_PR_NUMBER;
     console.log('初期化するぞ！');
     console.log(config);
   }
@@ -25,5 +38,24 @@ export class GitHubNotifierPlugin
     const { comparisonResult, reportUrl } = params;
     console.log('通知しちゃうぞ！');
     console.log(params);
+
+    const requestHeaders = {
+      Authorization: `token ${this._GITHUB_TOKEN}`,
+    };
+
+    const comments = await fetch(
+      `https://api.github.com/repos/${this._GITHUB_REPOSITORY}/issues/${this._GITHUB_PR_NUMBER}/comments`,
+      { headers: requestHeaders },
+    ).then((res) => res.json());
+    console.log(comments);
+
+    await fetch(
+      `https://api.github.com/repos/${this._GITHUB_REPOSITORY}/issues/${this._GITHUB_PR_NUMBER}/comments`,
+      {
+        method: 'post',
+        body: JSON.stringify({ body: reportUrl }),
+        headers: requestHeaders,
+      },
+    );
   }
 }
